74bd4f47ec3a40172cd05f4b990b7048ba719b1c
Amin Abbaspour <a.abbaspour@gmail.com>
2024-01-17 14:42:47 +1100

    Publish (#2)
    * tests for global. added readme
    
    * less dependency
    
    * bump publish version

---
+++ test/auth0-object.spec.js
@@ -0,0 +1,15 @@
+const wrapper = require('../src/rules-wrapper');
+
+const {
+    expect,
+    describe,
+    it,
+    jest: _jest,
+} = require('@jest/globals');
+
+describe('support auth0 embedded object', () => {
+    it('have auth0 object with valid access_token', () => {
+
+    });
+
+});
